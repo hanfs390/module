0d2b74bf34e5d66b253c7ca18965a785a84012a7
hanfushun <fushun.han@han-networks.com>
2018-11-30 17:24:32 +0800

    [demo2 finish]

---
+++ generic_netlink/demo3/kernel_genl.c
@@ -0,0 +1,213 @@
+//##include <net/sock.h>
+#include <linux/module.h>
+#include <linux/rtnetlink.h>
+#include <linux/netlink.h>
+#include <net/genetlink.h>
+#include <linux/string.h>
+/********************************the same with user**********************************************/
+/* attribute type */
+enum {
+        EXMPL_A_UNSPEC, /* default */
+        EXMPL_A_MSG,
+	EXMPL_A_PRINT,
+        __EXMPL_A_MAX,
+};
+#define EXMPL_A_MAX (__EXMPL_A_MAX - 1)
+/* commands */
+enum {
+        EXMPL_C_UNSPEC,
+        EXMPL_C_ECHO,
+	EXMPL_C_PRINT,
+        __EXMPL_C_MAX,
+};
+#define EXMPL_C_MAX (__EXMPL_C_MAX - 1)
+/************************************************************************************************/
+
+
+/* family definition */
+static struct genl_family family = {
+        .hdrsize = 0,
+        .name = "EXMPL",
+        .version = 2,
+        .maxattr = EXMPL_A_MAX,
+};
+
+
+
+/* 
+ * genl_register_family_with_ops_grps - assignemnt struct genl_ops to genl_family.ops and register family 
+ */
+static inline int
+_genl_register_family_with_ops_grps(struct genl_family *family,
+				    const struct genl_ops *ops, size_t n_ops,
+				    const struct genl_multicast_group *mcgrps,
+				    size_t n_mcgrps)
+{
+	family->module = THIS_MODULE; 
+	family->ops = ops;
+	family->n_ops = n_ops;
+	family->mcgrps = mcgrps;
+	family->n_mcgrps = n_mcgrps;
+	return genl_register_family(family);
+}
+#define genl_register_family_with_ops(family, ops)			\
+	_genl_register_family_with_ops_grps((family),			\
+					    (ops), ARRAY_SIZE(ops),	\
+					    NULL, 0)
+
+
+
+/* attribute policy */
+static struct nla_policy exmpl_genl_policy[EXMPL_A_MAX + 1] = {
+        [EXMPL_A_MSG] = { .type = NLA_STRING },
+        [EXMPL_A_PRINT] = { .type = NLA_STRING },
+};
+
+static int genl_fill_string_reply(struct sk_buff *msg, u32 portid, u32 seq, int flags, char * reply_data)
+{
+	void *hdr;
+
+	/* Add generic netlink header to netlink message */
+	hdr = genlmsg_put(msg, 0, seq, &family, flags, EXMPL_C_ECHO);
+	if (!hdr)
+		goto out;
+
+	rtnl_lock();
+	if (nla_put_string(msg, EXMPL_A_MSG, reply_data))
+		goto nla_put_failure;
+	rtnl_unlock();
+	genlmsg_end(msg, hdr);
+	return 0;
+
+nla_put_failure:
+	rtnl_unlock();
+	genlmsg_cancel(msg, hdr);
+out:
+	return -EMSGSIZE;
+}
+static int genl_fill_int_reply(struct sk_buff *msg, u32 portid, u32 seq, int flags, int reply)
+{
+	void *hdr;
+
+	/* Add generic netlink header to netlink message */
+	hdr = genlmsg_put(msg, 0, seq, &family, flags, EXMPL_C_PRINT);
+	if (!hdr)
+		goto out;
+
+	rtnl_lock();
+	if (nla_put_u32(msg, EXMPL_A_PRINT, reply))
+		goto nla_put_failure;
+	rtnl_unlock();
+	genlmsg_end(msg, hdr);
+	return 0;
+
+nla_put_failure:
+	rtnl_unlock();
+	genlmsg_cancel(msg, hdr);
+out:
+	return -EMSGSIZE;
+}
+/* doit handler */
+int exmpl_print(struct sk_buff *skb, struct genl_info *info)
+{
+	struct sk_buff *msg;
+	char * data = NULL;
+	int rc = -ENOBUFS;
+	data = nla_data(info->attrs[EXMPL_A_PRINT]);
+	if (data[nla_len(info->attrs[EXMPL_A_PRINT]) - 1] != '\0')
+		return -EINVAL;
+	printk("print : %s\n", data);
+	
+	/* alloc a netlink message */
+	msg = nlmsg_new(NLMSG_DEFAULT_SIZE, GFP_KERNEL);
+	if (!msg)
+		return rc;
+	rc = genl_fill_int_reply(msg, info->snd_portid, info->snd_seq, 0, 1);
+	if (rc < 0)
+		goto out_free;
+
+	return genlmsg_reply(msg, info);
+out_free:
+	nlmsg_free(msg);
+	return rc;
+}
+/* doit handler */
+int exmpl_echo(struct sk_buff *skb, struct genl_info *info)
+{
+	struct sk_buff *msg;
+	char * data = NULL;
+        char reply_data[10];
+	int rc = -ENOBUFS;
+ 
+        data = nla_data(info->attrs[EXMPL_A_MSG]);
+        if (data[nla_len(info->attrs[EXMPL_A_MSG]) - 1] != '\0')
+                return -EINVAL;
+        printk("recv the msg = %s\n", data);
+ 
+        strcpy(reply_data, data);
+        reply_data[strlen(data)] = '7';
+        reply_data[strlen(data) + 1] = '\0';
+        printk("reply_data = %s\n", reply_data);
+ 
+        /* alloc a netlink message */
+        msg = nlmsg_new(NLMSG_DEFAULT_SIZE, GFP_KERNEL);
+        if (!msg)
+                return rc;
+
+	rc = genl_fill_string_reply(msg, info->snd_portid, info->snd_seq, 0, reply_data);
+	if (rc < 0)
+		goto out_free;
+
+	return genlmsg_reply(msg, info);
+out_free:
+	nlmsg_free(msg);
+	return rc;
+}
+
+/* operation definition */
+struct genl_ops ops[] = { 
+	{
+	 	.cmd = EXMPL_C_ECHO,
+        	.flags = 0,
+        	.policy = exmpl_genl_policy,
+       		.doit = exmpl_echo,
+        	.dumpit = NULL,
+	},
+	{
+		.cmd = EXMPL_C_PRINT,
+		.flags = 0,
+        	.policy = exmpl_genl_policy,
+       		.doit = exmpl_print,
+        	.dumpit = NULL,
+	},
+};
+
+static int  __init genl_init(void)
+{	
+	int ret;		
+	ret = genl_register_family_with_ops(&family, ops);
+	if (ret) {
+		printk("register genl_family error=%d\n", ret);
+		return ret;
+	}
+	printk("genl_init\n");
+	return 0;	
+}	
+
+
+static void __exit genl_exit(void)
+
+{
+	genl_unregister_family(&family); 
+	printk("genl_exit\n");
+}
+
+
+module_init(genl_init);
+module_exit(genl_exit);
+
+
+MODULE_LICENSE("GPL");
+
+
+
---
+++ generic_netlink/demo3/user_genl.c
@@ -0,0 +1,267 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <stdlib.h>
+#include <errno.h>
+#include <poll.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#include <linux/genetlink.h>
+#include <netlink/attr.h>
+#include <netlink/genl/genl.h>
+#include <netlink/genl/ctrl.h>
+#define MAX_MSG_SIZE 128
+#define GENLMSG_DATA(glh) ((void *)(NLMSG_DATA(glh) + GENL_HDRLEN))
+#define NLA_DATA(na) ((void *)((char*)(na) + NLA_HDRLEN))
+
+/* netlink message */
+typedef struct msgtemplate {
+    struct nlmsghdr n;
+    struct genlmsghdr g;
+    char data[MAX_MSG_SIZE];
+} msgtemplate_t;
+
+
+
+/* attribute type */
+enum {
+        EXMPL_A_UNSPEC,
+        EXMPL_A_MSG,
+	EXMPL_A_PRINT,
+        __EXMPL_A_MAX,
+};
+#define EXMPL_A_MAX (__EXMPL_A_MAX - 1)
+
+/* cmd */
+enum {
+        EXMPL_C_UNSPEC,
+        EXMPL_C_ECHO,
+	EXMPL_C_PRINT,
+        __EXMPL_C_ECHO,
+};
+
+/* attribute policy */
+static struct nla_policy exmpl_genl_policy[EXMPL_A_MAX + 1] = {
+         [EXMPL_A_MSG] = { .type = NLA_STRING },
+         [EXMPL_A_PRINT] = { .type = NLA_U32 },
+};
+
+/*
+ * genl_rcv_msg - recv the msg from kernel module 
+ * @family_id :  genl family id 
+ * @sock : the sock of genl
+ * @data : the message from kernel
+ */
+void genl_rcv_msg(int family_id, int sock, void *data)
+{
+    int ret;
+    struct msgtemplate msg;
+    struct nlattr *na;
+    struct genlmsghdr *gnlh;
+    struct nlmsghdr *nlh;
+    struct nlattr *attrs[EXMPL_A_MAX + 1];
+    int len;
+
+    ret = recv(sock, &msg, sizeof(msg), 0);
+    if (ret < 0) {
+        return;
+    }
+    printf("received length %d\n", ret);
+
+    gnlh = &msg.g;
+    nlh = &msg.n;
+    genlmsg_parse(nlh, 0, attrs, EXMPL_A_MAX, exmpl_genl_policy);    
+    
+    switch (gnlh->cmd) {
+	case EXMPL_C_ECHO:
+		if (attrs[EXMPL_A_MSG]){
+			len = nla_len(attrs[EXMPL_A_MSG]);
+			memcpy(data, nla_data(attrs[EXMPL_A_MSG]), len);
+			printf("recevic data = %s\n", (char *)data);
+		}
+		break;
+	case EXMPL_C_PRINT:
+		if (attrs[EXMPL_A_PRINT]){
+			len = nla_len(attrs[EXMPL_A_PRINT]);
+			*((int *)data) = *((int *)nla_data(attrs[EXMPL_A_PRINT]));
+			printf("recevic data = %d\n", *((int*)data));
+		}
+		break;
+		
+
+    }
+
+} 
+
+/** 
+* genl_send_msg - 通过generic netlink给内核发送数据 
+*
+* @sd: 客户端socket 
+* @nlmsg_type: family_id
+* @nlmsg_pid: 客户端pid
+* @genl_cmd: 命令类型
+* @genl_version: genl版本号
+* @nla_type: netlink attr类型
+* @nla_data: 发送的数据
+* @nla_len: 发送数据长度
+*
+* return: 
+*    0:       成功 
+*    -1:      失败
+*/
+int genl_send_msg(int sd, u_int16_t nlmsg_type, u_int32_t nlmsg_pid,
+        u_int8_t genl_cmd, u_int8_t genl_version, u_int16_t nla_type,
+        void *nla_data, int nla_len)
+{
+    struct nlattr *na;
+    struct sockaddr_nl nladdr;
+    int r, buflen;
+    char *buf;
+
+	
+    msgtemplate_t msg;
+
+    printf("send is start\n");
+    if (nlmsg_type == 0) {
+        return 0;
+    }
+
+    msg.n.nlmsg_len = NLMSG_LENGTH(GENL_HDRLEN);
+    msg.n.nlmsg_type = nlmsg_type;
+    msg.n.nlmsg_flags = NLM_F_REQUEST;
+    msg.n.nlmsg_seq = 0;
+    /*
+     * nlmsg_pid是发送进程的端口号。
+     * Linux内核不关心这个字段，仅用于跟踪消息。
+     */
+    msg.n.nlmsg_pid = nlmsg_pid;
+    msg.g.cmd = genl_cmd;
+    msg.g.version = genl_version;
+    na = (struct nlattr *) GENLMSG_DATA(&msg);
+    na->nla_type = nla_type;
+    na->nla_len = nla_len + 1 + NLA_HDRLEN;
+    memcpy(NLA_DATA(na), nla_data, nla_len);
+    msg.n.nlmsg_len += NLMSG_ALIGN(na->nla_len);
+
+    printf("send msg : %s\n", (char *)nla_data);
+    buf = (char *) &msg;
+    buflen = msg.n.nlmsg_len ;
+    memset(&nladdr, 0, sizeof(nladdr));
+    nladdr.nl_family = AF_NETLINK;
+    while ((r = sendto(sd, buf, buflen, 0, (struct sockaddr *) &nladdr
+            , sizeof(nladdr))) < buflen) {
+        if (r > 0) {
+            buf += r;
+            buflen -= r;
+        } else if (errno != EAGAIN) {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+static int genl_get_family_id(int sd, char *family_name)
+{
+    msgtemplate_t ans;
+    int id, rc;
+    struct nlattr *na;
+    int rep_len;
+
+    rc = genl_send_msg(sd, GENL_ID_CTRL, 0, CTRL_CMD_GETFAMILY, 1,
+                    CTRL_ATTR_FAMILY_NAME, (void *)family_name,
+                    strlen(family_name)+1);
+
+
+    rep_len = recv(sd, &ans, sizeof(ans), 0);
+    if (rep_len < 0) {
+        return 0;
+    }
+    if (ans.n.nlmsg_type == NLMSG_ERROR || !NLMSG_OK((&ans.n), rep_len)) {
+        return 0;
+    }
+
+    na = (struct nlattr *) GENLMSG_DATA(&ans);
+    na = (struct nlattr *) ((char *) na + NLA_ALIGN(na->nla_len));
+    if (na->nla_type == CTRL_ATTR_FAMILY_ID) {
+        id = *(__u16 *) NLA_DATA(na);
+    } else {
+        id = 0;
+    }
+   printf("id = %d\n", id);
+    return id;
+}
+
+int  main(void)
+{
+    struct sockaddr_nl saddr;
+    int                sock;
+    int id;
+    char * send_msg = "123456";
+    int ret;
+    char * data;
+    void * reply; /* the attrs */
+    char * print_data = "Hello Word !";
+    reply = malloc(32);
+
+    sock = socket(AF_NETLINK, SOCK_RAW, NETLINK_GENERIC);
+
+    if (sock < 0) {
+        return -1;
+    }
+
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.nl_family = AF_NETLINK;
+    saddr.nl_pid = 1234;
+    if (bind(sock, (struct sockaddr*)&saddr, sizeof(saddr)) < 0) {
+        printf("bind fail!\n");
+        close(sock);
+        return -1;
+    }
+    
+    /* self-defining function to get family id */
+    id = genl_get_family_id(sock, "EXMPL");
+    if (id <= 0) {
+	printf("maybe kernel register failed ! can`t find family ID\n");
+    }
+    
+    printf("find family ID = %d by family name\n", id);
+    data =(char*)malloc(256);
+    if(!data)
+    {
+        perror("malloc error!");
+        exit(1);
+    }
+    memset(data,0,256);
+    strcpy(data,"123456");
+    
+    ret = genl_send_msg(sock, id, 1234, EXMPL_C_ECHO, 1, EXMPL_A_MSG, (void *)data, strlen(data) + 1);
+    if (ret) {
+	printf("send failed\n");
+    }
+
+    printf("send success\n");
+    genl_rcv_msg(id, sock, reply);
+    printf("recv mesg = %d\n", strlen((char*)reply));    
+
+
+    strcpy(data, print_data);
+    printf("kernel print :%s\n", print_data);
+    ret = genl_send_msg(sock, id, 1234, EXMPL_C_PRINT, 1, EXMPL_A_PRINT, (void *)data, strlen(data) + 1);
+    if (ret) {
+	printf("send failed\n");
+    }
+    printf("send success\n");
+    genl_rcv_msg(id, sock, reply);
+   
+    return 0; 
+
+}
+
+
+
+
+
